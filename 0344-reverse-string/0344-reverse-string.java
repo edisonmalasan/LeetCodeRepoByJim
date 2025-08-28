d6651ec6b78a55625ad5e17618a0c30fc3c3e40e
Edison <163733802+edisonmalasan@users.noreply.github.com>
2025-08-28 09:02:42 +0800

    Update 0344-reverse-string.java

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,3 +1,14 @@
+/**
+Algorithm:
+1. Initialize two pointers: set left = 0 and right = s.length - 1.
+2. While left < right, do the following:
+3. Swap s[left] and s[right].
+4. Increment left by 1.
+5. Decrement right by 1.
+7. Repeat until the two pointers meet or cross.
+*/
+    
+
 class Solution {
     public void reverseString(char[] s) {
         if (s.length == 0) return;
